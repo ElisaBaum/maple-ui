de92723fb7319c1a60e3097587b4c143487554ac
RobinBuschmann <robinbuschmann@hotmail.de>
2017-11-06 20:17:35 +0100

    some restructuring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
+import {Component} from 'react';
+import {Inject, Module} from 'react.di';
+import {Switch, Route, Router, Redirect} from 'react-router';
 import {AuthInterceptor} from './auth/AuthInterceptor';
 import {APIInterceptor} from './common/APIInterceptor';
-import {Module} from 'react.di';
 import {INTERCEPTOR_TOKEN} from './http/HttpInterceptor';
 import {HISTORY_TOKEN, history} from './common/history';
 import {AuthService} from './auth/AuthService';
-import {Main} from './Main';
 import {UserHttpService} from './user/UserHttpService';
 import {Http} from './http/Http';
+import {center} from './layout/decorators/center/center';
+import {LoginContainer} from './login/LoginContainer';
+import {Skeleton} from './skeleton/Skeleton';
+import {Toast} from './layout/components/toast/Toast';
 import './App.scss';
 
 export const App = () => (
@@ -19,6 +24,30 @@ export const App = () => (
     {provide: INTERCEPTOR_TOKEN, useClass: APIInterceptor},
     {provide: INTERCEPTOR_TOKEN, useClass: AuthInterceptor},
   ]}>
-    <Main/>
+    <Content/>
   </Module>
 );
+
+class Content extends Component {
+  @Inject(HISTORY_TOKEN) history: History;
+  @Inject authService: AuthService;
+
+  render() {
+    return (
+      <div className={'root-container'}>
+        <Router history={this.history}>
+          <Switch>
+            <Route path={'/login'} render={() => center(LoginContainer)}/>
+            <Route path={'/'} render={() => {
+              if (this.authService.getValidToken()) {
+                return <Skeleton/>;
+              }
+              return <Redirect to={'/login'}/>;
+            }}/>
+          </Switch>
+        </Router>
+        <Toast/>
+      </div>
+    );
+  }
+}
