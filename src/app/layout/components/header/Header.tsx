9ef31c8ace53e716475598401837c69ca4337ac4
Elisa Baum <elisabaum@hotmail.com>
2017-11-16 21:17:40 +0100

    added default image in header

--- src/app/layout/components/header/Header.tsx
+++ src/app/layout/components/header/Header.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import './Header.scss';
-import * as test from './test.jpg';
 
 export const Header = () => (
     <div className={'meta-container'}>
-      <img src={test} className="angle__content"/>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Sig07-007.jpg/1280px-Sig07-007.jpg"
+           alt="Nebula"
+           className="angle__content"/>
     </div>
 );
