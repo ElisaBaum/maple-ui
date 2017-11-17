9a1e8bac49119ab2a8f9f29b3f75a6316c4bc5a6
Elisa Baum <elisabaum@hotmail.com>
2017-11-17 14:16:32 +0100

    clean up

--- src/app/journey/Journey.tsx
+++ src/app/journey/Journey.tsx
@@ -1,21 +1,13 @@
 import * as React from 'react';
 import {ContentContainer} from "../layout/components/content-container/ContentContainer";
 import {JourneyData} from "./JourneyData";
-import {Component} from "react";
 
 interface JourneyProps {
   content: JourneyData;
 }
 
-export class Journey extends Component<JourneyProps> {
-
-  render() {
-    const {headline, headlineIcon, journeyDescription} = this.props.content;
-
-    return (
-      <ContentContainer headline={headline} headlineIcon={headlineIcon}>
-        <div>{journeyDescription}</div>
-      </ContentContainer>
-    );
-  }
-}
+export const Journey = ({content}: JourneyProps) => (
+  <ContentContainer headline={content.headline} headlineIcon={content.headlineIcon}>
+    <div>{content.journeyDescription}</div>
+  </ContentContainer>
+);
