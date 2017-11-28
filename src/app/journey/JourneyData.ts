d067c14a0095eb42e66358d02328de5088239c89
Elisa Baum <elisabaum@hotmail.com>
2017-11-28 11:02:19 +0100

    * add Map component to display google maps * improve Journey component

--- src/app/journey/JourneyData.ts
+++ src/app/journey/JourneyData.ts
@@ -1,7 +1,28 @@
 import {ContentData} from "../dynamic-content/ContentData";
 
 export interface JourneyData extends ContentData {
+  locationAddress: LocationAddress;
 
-  journeyDescription: string;
+  journeyByPublicTransport: JourneyByTransportation;
+  journeyByCar: JourneyByTransportation;
 
+  apiToken: string;
+}
+
+export interface LocationAddress extends ContentData {
+  name: string;
+  street: string;
+  houseNumber: string;
+  postCode: string;
+  city: string;
+  gpsCoordinates: GPSCoordinates;
+}
+
+export interface GPSCoordinates extends ContentData {
+  longitude: number;
+  latitude: number;
+}
+
+export interface JourneyByTransportation extends ContentData {
+  description: string;
 }
