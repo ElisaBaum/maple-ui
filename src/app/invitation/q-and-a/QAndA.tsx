3689549dfc08aca35613e3f417d8764ce7ba23ff
Elisa Baum <elisabaum@hotmail.com>
2018-01-21 19:13:06 +0100

    radio button styling and q&a icon

--- src/app/invitation/q-and-a/QAndA.tsx
+++ src/app/invitation/q-and-a/QAndA.tsx
@@ -12,7 +12,7 @@ export function QAndA({content}: ContentComponentProps<QAndAData>) {
       {
         questions.map((question, index) => (
           <Card key={index}>
-            <Headline text={question.question}/>
+            <Headline text={question.question} icon="question_answer"/>
             <Item>
               {question.answer}
             </Item>
