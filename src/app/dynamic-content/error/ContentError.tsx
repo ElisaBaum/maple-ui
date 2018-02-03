d04991f5620c35156a646abb74303e0f17086d8a
Elisa Baum <elisabaum@hotmail.com>
2018-02-03 21:05:41 +0100

    not found page

--- src/app/dynamic-content/error/ContentError.tsx
+++ src/app/dynamic-content/error/ContentError.tsx
@@ -1,27 +1,21 @@
 import * as React from 'react';
-import './ContentError.scss';
 import {Form} from "../../layout/components/form/Form";
 import {Item} from "../../layout/components/item/Item";
 import {FormButton} from "../../layout/components/form/form-button/FormButton";
-import {Logo} from "../../layout/components/logo/Logo";
-import {centered} from "../../layout/decorators/center/center";
+import {CenteredError} from "../../layout/components/error/Error";
 
 interface ContentErrorProps {
   reload();
 }
 
-export const CenteredContentError = centered(ContentError);
-
-function ContentError({reload}: ContentErrorProps) {
+export function ContentError({reload}: ContentErrorProps) {
   return (
-    <div className="content-error">
-      <Logo className="logo"/>
-      <div>Es ist ein Fehler beim Laden aufgetreten.</div>
+    <CenteredError message="Es ist ein Fehler beim Laden aufgetreten.">
       <Form onSubmit={() => reload()}>
         <Item>
           <FormButton type="inverse">Erneut versuchen</FormButton>
         </Item>
       </Form>
-    </div>
+    </CenteredError>
   );
 }
