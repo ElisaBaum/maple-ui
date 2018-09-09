a8491695e229d26b61417c02bf0dcaa906e7a056
RobinBuschmann <robinbuschmann@hotmail.de>
2018-09-09 18:27:50 +0200

    Uses gallery info text from backend

--- src/app/gallery/gallery-sections/gallery-sections/GallerySections.tsx
+++ src/app/gallery/gallery-sections/gallery-sections/GallerySections.tsx
@@ -2,15 +2,15 @@ import * as React from 'react';
 import {GallerySectionTile} from '../GallerySectionTile';
 import {Item, LinkItem} from '../../../layout/components/item/Item';
 import {Button} from '../../../layout/components/button/Button';
-import './GallerySections.scss';
 import {Card} from '../../../layout/components/card/Card';
 import {Headline} from '../../../layout/components/headline/Headline';
+import './GallerySections.scss';
 
 export const GallerySections = ({user, sections, content, onCreateSection, onDeleteSection}) => (
   <Card className={'gallery-sections'}>
     <Headline text={'Hallo {{username}}'.replace('{{username}}', user.name)} icon={'favorite'}/>
     <Item>
-      Hier kannst du dir alle Bilder unserer Hochzeit ansehen und selbst Neue hochladen.
+      {content.info}
     </Item>
     <Item>
       <Button htmlType={'button'}
